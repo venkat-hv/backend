8c1661c631279143d5cdfa51b5c8c8ee99329d4c
Sai Venkat <kondeti.venkat@herovired.com>
2024-07-16 12:00:00 +0530

    test 1

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-const express = require('express')
+const express = require("express");
 const bodyParser = require("body-parser");
-const session = require('express-session');
+const session = require("express-session");
 
-const app = express()
-const port = 8080
-const cors = require('cors');
+const app = express();
+const port = 8080;
+const cors = require("cors");
 
-
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: ["https://669512c91a375517b959417e--startling-alpaca-4ff4e4.netlify.app"],
-    methods: ["POST", "GET"]
-}));
+    origin: [
+      "https://669512c91a375517b959417e--startling-alpaca-4ff4e4.netlify.app",
+    ],
+    methods: ["POST", "GET"],
+  })
+);
 
-app.use(session({
-    secret: "mysecret", 
+app.use(
+  session({
+    secret: "mysecret",
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 1000 * 60 * 60, // Session expires after 1hr
-        sameSite: 'none',
-        secure: true,
-        httpOnly: true // Ensures cookie is not accessible via JavaScript
-        }
-}));
+      maxAge: 1000 * 60 * 60, // Session expires after 1hr
+      // sameSite: "none",
+      // secure: true,
+      // httpOnly: true, // Ensures cookie is not accessible via JavaScript
+    },
+  })
+);
 
-app.use(express.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-app.post('/capture', async(req,res)=>{
-    if(req.session.user){
-        return res.json({message: "session already exist!"})
-    }
-    const user = req.body.otherData
-    req.session.user = user
-    return res.json({message: "session created!"})
-})
-app.get('/validate', async(req,res)=>{
-    if(!req.session.user){
-        return res.json({message: "session does not exist"})
-    }
-    return res.json(req.session.user)
-})
+app.post("/capture", async (req, res) => {
+  if (req.session.user) {
+    return res.json({ message: "session already exist!" });
+  }
+  const user = req.body.otherData;
+  req.session.user = user;
+  return res.json({ message: "session created!" });
+});
 
-app.listen(port, ()=>{
-    console.log(`Sever is running at ${port}`)
-})
+app.get("/validate", async (req, res) => {
+  if (!req.session.user) {
+    return res.json({ message: "session does not exist" });
+  }
+  return res.json(req.session.user);
+});
+
+app.listen(port, () => {
+  console.log(`Sever is running at ${port}`);
+});
